e27713a04f101ab027c12a83213ac7bf89607ebe
Robyn <127926809+Robyn-web@users.noreply.github.com>
2023-09-28 21:32:59 +0100

    Responsive Testing

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -21,9 +21,9 @@ export default function WeatherForecastDay(props) {
     return days[day];
   }
   return (
-    <div>
+    <div className="WeatherForecast">
       <div className="WeatherForecast-day">{day()}</div>
-      <WeatherIcon code={props.data.weather[0].icon} size={36} />
+      <WeatherIcon code={props.data.weather[0].icon} size={25} />
       <div className="WeatherForecast-temperature">
         <span className="WeatherForecast-temperature-max">
           {maxTemperature()}C
